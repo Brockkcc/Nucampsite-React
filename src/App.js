dfcff19c16be09023deb5d7ebc1c78e2bd47094f
Brock Carless <brock.carless@gmail.com>
2023-02-11 13:30:57 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesList />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
